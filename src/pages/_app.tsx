59ee6ee2efe3915d4b6029081e290ac01f93ba47
Jonatas Sousa <jonatasbraz@gmail.com>
2022-01-31 23:21:34 -0300

    adicionando global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,23 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import GlobalStyles from 'styles/global';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }: AppProps) {
+  /** TODO favicon **/
+
+  return (
+    <>
+      <Head>
+        <title>Next JS - Boilerplate</title>
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp;
+export default App;
